b71bdbcb64499b31cfbd5ae0919ca6f6f6908aa3
Jocelyn Peña <jocelyn.p.reyes332b@gmail.com>
2023-11-29 20:29:08 -0600

    Se agrega revision de codigo 2

--- formulario.js
+++ formulario.js
@@ -1,31 +1,30 @@
-var formulario = document.querySelector("#form")
+var formulario = document.querySelector(".formulario") //se cambio("#form") por (".formulario")
 
 formulario.onsubmit = function(e) {
 
   e.prevent();
   
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
+  var nombreFormulario = formulario.elements[0]; //se agrega (;) y se cambia el nombre de la variable para que sea mas claro
+  var edadFormulario = formulario.elements[1]; //se agrega (;) y se cambia el nombre de la variable para que sea mas claro
+  var nacionalidadFormlario = formulario.elements[2]; //se agrega (;) y se cambia el nombre de la variable para que sea mas claro
 
-  var nombre = n.value
-  var edad = e.value
+  var nombre = nombreFormulario.value; //se agrega (;)
+  var edad = edadFormulario.value; //se agrega (;)
 
-  var i = na.selectedIndex
-  var nacionalidad = na.options[i].value
-  console.log(nombre, edad)
-  console.log(nacionalidad)
+  var i = nacionalidadFormlario.selectedIndex; //se agrega (;)
+  var nacionalidad = nacionalidadFormlario.options[i].value; //se agrega (;)
+  
+  console.log(nombre, edad); //se agrega (;)
+  console.log(nacionalidad); //se agrega (;)
 
   if (nombre.length === 0) {
-    n.classList.add("error")
+    nombreFormulario.classList.add("error"); //se agrega (;)
   }
   if (edad < 18 || edad > 120) {
-    e.classList.add("error")
+    edadFormulario.classList.add("error"); //se agrega (;)
   }
 
-if (nombre.length > 0 
-  && (edad > 18 
-    && edad < 120) ) {
+if (nombre.length > 0  && (edad > 18   && edad < 120) ) { //se acomodo en una sola linea 
   agregarInvitado(nombre, edad, nacionalidad)
   }
 }
@@ -52,30 +51,34 @@ function agregarInvitado(nombre, edad, nacionalidad) {
     nacionalidad = "Peruana"
   }
 
-var lista = document.getElementById("lista-de-invitados")
+var lista = document.getElementById("lista-de-invitados"); //se agrega (;)
 
-var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
-lista.appendChild(elementoLista)
+var elementoLista = document.createElement("div"); //se agrega (;)
+elementoLista.classList.added("elemento-lista"); //se agrega (;)
+lista.appendChild(elementoLista); //se agrega (;)
 
+
+//este fragmento de codigo tiene elementos repetidos
+/*
 var spanNombre = document.createElement("span")
 var inputNombre = document.createElement("input")
 var espacio = document.createElement("br")
 spanNombre.textContent = "Nombre: "
-inputNombre.value = nombre 
+inputNombre.value = nombre
 elementoLista.appendChild(spanNombre)
 elementoLista.appendChild(inputNombre)
 elementoLista.appendChild(espacio)
+*/
 
 function crearElemento(descripcion, valor) {
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
+var spanNombre = document.createElement("span"); //se agrega (;)
+var inputNombre = document.createElement("input"); //se agrega (;)
+var espacio = document.createElement("br"); //se agrega (;)
 spanNombre.textContent = descripcion + ": "
-inputNombre.value = valor 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
+inputNombre.value = valor; //se agrega (;)
+elementoLista.appendChild(spanNombre); //se agrega (;)
+elementoLista.appendChild(inputNombre); //se agrega (;)
+elementoLista.appendChild(espacio); //se agrega (;)
 }
 
 crearElemento("Nombre", nombre)
@@ -86,12 +89,13 @@ crearElemento("Nacionalidad", nacionalidad)
 var botonBorrar = document.createElement("button")
 botonBorrar.textContent = "Eliminar invitado"
 botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-elementoLista.appendChild(corteLinea)
+
+var corteLinea = document.createElement("br"); //se agrega (;)
+elementoLista.appendChild(corteLinea); //se agrega (;)
 elementoLista.appendChild(botonBorrar);
 
  botonBorrar.onclick = function() {
 // this.parentNode.style.display = 'none';
-botonBorrar.parentNode.remove()
+botonBorrar.parentNode.remove(); //se agrega (;)
   }
 }
